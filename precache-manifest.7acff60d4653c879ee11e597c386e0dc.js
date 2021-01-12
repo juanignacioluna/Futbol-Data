e6a9aab6d922dcf9a00e1da53df60100ff607f82
Juan Ignacio Luna <chinoluna11@gmail.com>
2021-01-12 19:04:16 -0300

    Updates

---
+++ precache-manifest.7acff60d4653c879ee11e597c386e0dc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ee9393a6b503e148c5b31ea95833dc",
+    "url": "/Futbol-Data/index.html"
+  },
+  {
+    "revision": "fbba0212f9dd18c2ffc5",
+    "url": "/Futbol-Data/static/css/main.073fbc90.chunk.css"
+  },
+  {
+    "revision": "1900feeb559232f7841f",
+    "url": "/Futbol-Data/static/js/2.99e107cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Futbol-Data/static/js/2.99e107cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbba0212f9dd18c2ffc5",
+    "url": "/Futbol-Data/static/js/main.d59aa0a9.chunk.js"
+  },
+  {
+    "revision": "668ef9e80a4ffd647720",
+    "url": "/Futbol-Data/static/js/runtime-main.ff45c4f6.js"
+  },
+  {
+    "revision": "85f98c37ca46ee50b5c8ba121baacf2d",
+    "url": "/Futbol-Data/static/media/alemania.85f98c37.svg"
+  },
+  {
+    "revision": "87e4c3aa6fb5b73f6680137c33ffc5dd",
+    "url": "/Futbol-Data/static/media/equipos.87e4c3aa.png"
+  },
+  {
+    "revision": "e511dceedff1a2f71233ed3907c73199",
+    "url": "/Futbol-Data/static/media/espana.e511dcee.svg"
+  },
+  {
+    "revision": "2f9befe94ef9076d58b0a2ae38e1a025",
+    "url": "/Futbol-Data/static/media/francia.2f9befe9.svg"
+  },
+  {
+    "revision": "85545a9416dbc214e98593d195f27b19",
+    "url": "/Futbol-Data/static/media/goleadores.85545a94.png"
+  },
+  {
+    "revision": "d14795a9ff933f1fb5e0513496017be0",
+    "url": "/Futbol-Data/static/media/inglaterra.d14795a9.svg"
+  },
+  {
+    "revision": "41b03921340062a6a1727eb440d19a8b",
+    "url": "/Futbol-Data/static/media/italia.41b03921.svg"
+  }
+]);
